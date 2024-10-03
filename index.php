a024dd8562a4cf090899db052c2bf841685758fb
DanieleChecchin <danielechecchin26@gmail.com>
2024-10-03 15:07:38 +0200

    dati stampati in pagina

--- index.php
+++ index.php
@@ -1,3 +1,48 @@
+<?php
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,8 +56,19 @@
 
 <body>
 
-    <main class="container my-5 text-center">
-        <h1>HOTEL</h1>
+    <main class="container my-5">
+        <h1 class="text-center">HOTEL</h1>
+
+        <ul class="list-group">
+            <?php foreach ($hotels as $singleHotel) { ?>
+            <li class="list-group-item">
+                <h3> <?= $singleHotel['name']; ?> </h3>
+                <p> <?= $singleHotel['description']; ?> </p>
+                <h5> <?= $singleHotel['vote']; ?> </h5>
+                <h5> <?= $singleHotel['distance_to_center']; ?> </h5>
+            </li>
+            <?php } ?>
+        </ul>
     </main>
 
 </body>
